69aec08e9aa97ad52d34d3f31be08de01c94d0e9
Stu.P <stuartparton@gmail.com>
2016-07-04 21:30:35 +1000

    move some files around

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var scraperApp_1 = require('./scraperApp');
-var config_1 = require('./config');
-var appConfig = new config_1.Config();
-var scrapperApp = new scraperApp_1.ScraperApp(appConfig);
-scrapperApp.start();
-//# sourceMappingURL=index.js.map
